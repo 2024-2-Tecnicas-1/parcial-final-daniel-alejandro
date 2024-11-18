de6ec5b00ec8d5cea6240d7ab27283fde8b03739
anonim <anonim@gmail.com>
2024-11-18 10:23:26 -0500

    prueba

--- src/main/java/logicaNegocio/Animal.java
+++ src/main/java/logicaNegocio/Animal.java
@@ -6,7 +6,27 @@ public abstract class Animal {
     
     
     public abstract String obtenerDieta();
-        
+    
+    public static void main(String[] args) {
+        Animal leon = new Leon();
+        Animal delfin = new Delfin();
+        Perro perro = new Perro();
+
+        System.out.println("León:");
+        System.out.println("Sonido: " + leon.emitirSonido());
+        System.out.println("Dieta: " + leon.obtenerDieta());
+   
+
+        System.out.println("Delfín:");
+        System.out.println("Sonido: " + delfin.emitirSonido());
+        System.out.println("Dieta: " + delfin.obtenerDieta());
+
+
+        System.out.println("Perro:");
+        System.out.println("Sonido: " + perro.emitirSonido());
+        System.out.println("Dieta: " + perro.obtenerDieta());
+        System.out.println("Interacción con humanos: " + perro.interactuarConHumano());
+    }
     
     
 }
